7d6f069879ffcd1d9d402b400b5a4558e7dacef8
bongiBong-create <mairanss543@inbox.ru>
2024-11-30 00:59:25 +0300

    fsd

---
+++ src/app/index.jsx
@@ -0,0 +1,15 @@
+import { AdminDashboard } from "../pages/admin-dashboard"
+import { ProfileDashboard } from "../pages/profile-dashboard"
+import { SignIn } from "../pages/sign-in"
+
+function App() {
+  return (
+    <>
+    <AdminDashboard />
+    <ProfileDashboard />
+    <SignIn />
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/profile-dashboard/ui/index.jsx
@@ -0,0 +1,3 @@
+export const ProfileDashboard = () => {
+  return <>ProfileDashboard</>
+}
